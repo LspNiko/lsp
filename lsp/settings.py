2f97e95d04a9b8e3ba555b6f07ce6561be90df3b
mykola <m.trohanenko2@gmail.com>
2025-07-17 03:55:28 +0300

    створив програму cart та заповнив models.py та cart.py

--- lsp/settings.py
+++ lsp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'main',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 86400
+SESSION_SAVE_EVERY_REQUEST = True
